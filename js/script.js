7efc969cac2414b95ecdeb537c7758961bfa8025
maurizio-marotta <mauriziomarotta@live.it>
2023-03-17 15:06:56 +0100

    [finish]

--- js/script.js
+++ js/script.js
@@ -2,5 +2,26 @@
 1) Chiedi all’utente il suo nome,
 2) chiedi il suo cognome,
 3) chiedi il suo colore preferito
-4) scrivi sulla pagina nome cognome colore preferito
+4) chiedi la sua età
+5) scrivi sulla pagina nome cognome colore preferito
 */
+
+const nome = prompt("inserire nome");
+
+const cognome = prompt("inserisci cognome");
+
+const colorePreferito = prompt("colore preferito?");
+
+const età = prompt("inserire età");
+
+console.log(nome, cognome, colorePreferito, età);
+
+const output =`
+ciao ${nome} ${cognome} <br>
+il tuo colore preferito è ${colorePreferito} <br>
+e la ta età è ${età}
+`
+
+console.log(output);
+
+document.getElementById("output").innerHTML = output;
